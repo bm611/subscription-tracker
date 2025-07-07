d20d66f0f033507a8c549bb6428ddcd98ee3c547
bm611 <bharath.mohan.pro@gmail.com>
2025-07-07 13:02:37 -0700

    revert changes to caddyfile

--- rxconfig.py
+++ rxconfig.py
@@ -14,11 +14,9 @@ config = ReflextemplateConfig(
     frontend_port=3000,  # default frontend port
     backend_port=8000,  # default backend port
     # For production on Railway, we need to use the public domain
-    api_url=f"https://{os.environ[railway_domain]}"
+    api_url=f"https://{os.environ[railway_domain]}/backend"
     if railway_domain in os.environ
-    else "http://localhost:8000",
-    # Set backend host to bind to all interfaces in production
-    backend_host="0.0.0.0" if railway_domain in os.environ else "127.0.0.1",
+    else "http://0.0.0.0:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
