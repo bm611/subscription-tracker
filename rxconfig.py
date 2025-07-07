c4a2d92ae7c2f4d9f933672956f219937a67d57f
bm611 <bharath.mohan.pro@gmail.com>
2025-07-06 21:30:34 -0700

    railway deploy config

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,25 @@
 import reflex as rx
+import os
 
-config = rx.Config(
+railway_domain = "RAILWAY_PUBLIC_DOMAIN"
+
+
+class ReflextemplateConfig(rx.Config):
+    pass
+
+
+config = ReflextemplateConfig(
     app_name="recsub",
+    telemetry_enabled=False,
+    frontend_port=3000,  # default frontend port
+    backend_port=8000,  # default backend port
+    # use https and the railway public domain with a backend route if available, otherwise default to a local address
+    api_url=f"https://{os.environ[railway_domain]}/backend"
+    if railway_domain in os.environ
+    else "http://0.0.0.0:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
+    show_built_with_reflex=False,
 )
