7b852e2b097e4a8a54e3479fb323e46299cc6860
bm611 <bharath.mohan.pro@gmail.com>
2025-07-08 19:56:27 -0700

    landing page button update

--- recsub/pages/landing.py
+++ recsub/pages/landing.py
@@ -39,21 +39,27 @@ def landing_page() -> rx.Component:
                                 class_name="flex items-center justify-center px-3 py-2 sm:px-8 sm:py-4 bg-indigo-400 text-white rounded-xl hover:bg-indigo-500 transition-all duration-300 group shadow-lg hover:shadow-xl transform hover:scale-105 h-10 sm:h-auto",
                             ),
                             href="/demo",
-                            class_name="inline-block mr-3 sm:mr-4 flex-1 sm:flex-none no-underline",
+                            class_name="inline-block w-full sm:w-auto sm:mr-4 no-underline",
                         ),
                         # Sign In Button
                         rx.link(
                             rx.el.div(
+                                rx.el.div(
+                                    rx.html(
+                                        '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path></svg>'
+                                    ),
+                                    class_name="w-5 h-5 mr-2 flex items-center justify-center",
+                                ),
                                 rx.el.span(
-                                    "Sign In",
+                                    "Sign in with Google",
                                     class_name="text-base sm:text-lg font-medium whitespace-nowrap",
                                 ),
                                 class_name="flex items-center justify-center px-3 py-2 sm:px-8 sm:py-4 bg-transparent border-2 border-indigo-400 text-indigo-500 rounded-xl hover:bg-indigo-400 hover:text-white transition-all duration-300 shadow-lg hover:shadow-xl transform hover:scale-105 h-10 sm:h-auto",
                             ),
                             href="/signin",
-                            class_name="inline-block flex-1 sm:flex-none no-underline",
+                            class_name="inline-block w-full sm:w-auto no-underline",
                         ),
-                        class_name="flex flex-row items-center w-full sm:w-auto",
+                        class_name="flex flex-col sm:flex-row items-center w-full sm:w-auto space-y-3 sm:space-y-0 mx-auto sm:mx-0",
                     ),
                     class_name="text-left max-w-lg mb-24",
                 ),
